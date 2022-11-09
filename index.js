239f17c95e0d764d4e56f8d54038156ca7fcbc02
unknown <khorshedjhoni@gmail.com>
2022-11-09 12:04:10 +0600

    single service get add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion,} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId,} = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -29,6 +29,12 @@ async function run (){
         const query = {}
         const cursor = serviceCollection.find(query)
         const services = await cursor.toArray();
+        res.send(services)
+    })
+    app.get('/service/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const services= await serviceCollection.findOne(query)
         res.send(services)
     })
 
