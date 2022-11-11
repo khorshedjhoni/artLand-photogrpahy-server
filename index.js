9615ed42a3b21906a5a0df6cac4e6c5c5630fba1
unknown <khorshedjhoni@gmail.com>
2022-11-11 21:16:41 +0600

    review delete

--- index.js
+++ index.js
@@ -33,12 +33,6 @@ async function run (){
         const services = await cursor.toArray();
         res.send(services)
     })
-    app.get('/reviews', async(req,res)=>{   
-        const query = {}
-        const cursor = reviewsCollection.find(query)
-        const services = await cursor.toArray();
-        res.send(services)
-    })
     app.get('/service/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
@@ -50,10 +44,68 @@ async function run (){
         const result =await serviceCollection.insertOne(order);
         res.send(result)
     })
+    // review-part
+
+    app.post('/reviews',async(req,res)=>{
+        const reviews = req.body
+        const result = await reviewsCollection.insertOne(reviews)
+        res.send(result)
+    })
+
+    // review by email
+    app.get('/reviews',async(req,res)=>{   
+        let query = {}
+
+        if(req.query.email){
+            query={
+                email:req.query.email
+            }
+        }
+        const cursor = reviewsCollection.find(query)
+        const reviews = await cursor.toArray();
+        res.send(reviews)
+    })
+
+    // review by id 
+    app.get('/review',async(req,res)=>{   
+        let query = {}
+
+        if(req.query.service){
+            query={
+                service:req.query.service
+            }
+        }
+        const cursor = reviewsCollection.find(query).limit(0).sort({$natural:-1})
+        const reviews = await cursor.toArray();
+        res.send(reviews)
+    })
+
+    // rev update
+    app.patch('/reviews/:id',async(req,res)=>{
+        const id = req.params.id 
+        const status = req.body.status
+        const query = {_id: ObjectId(id)}
+        const updatedDoc = {
+            $set:{
+                status:status
+            }
+        }
+        const result = await reviewsCollection.updateOne(query,updatedDoc)
+        res.send(result)
+    })
+    // rev delete
+
+    app.delete('/reviews/:id',async(req,res)=>{
+        const id = req.params.id 
+        const query = {_id:ObjectId(id)}
+        const result = await reviewsCollection.deleteOne(query)
+        res.send(result)
+    })
+    
 
    }
    finally{
-    // 
+    //  
    }
 
 }
